db378e86619e99ce6b79578327bf4839a0c03ece
keke <keke@kekedeMacBook-Air.local>
2020-10-09 23:44:50 +0800

    feat:  增加了判断类型工具方法

--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -1,2 +1,23 @@
 export const normalizePath = (path: string): string =>
   path.startsWith("/") ? path : `/${path}`;
+
+export const isUndefined = (val: unknown): val is undefined =>
+  typeof val === "undefined";
+
+export const isNil = (val: unknown): val is null | undefined =>
+  isUndefined(val) || val === null;
+
+export const isObject = (val: unknown): val is object =>
+  !isNil(val) && typeof val === "object";
+
+export const isString = (val: unknown): val is string =>
+  typeof val === "string";
+
+export const isConstructor = (val: unknown): boolean => val === "constructor";
+
+export const isEmpty = (val: any): boolean => val?.length === 0;
+
+export const isSymbol = (fn: any): boolean => typeof fn === "symbol";
+
+export const isPlainObject = (obj: unknown): boolean =>
+  Object.prototype.toString.call(obj) === "[object Object]";
