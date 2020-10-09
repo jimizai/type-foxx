73812bf9f2269d456df003b0b084d3a9c22b20e5
keke <keke@kekedeMacBook-Air.local>
2020-10-09 23:45:44 +0800

    refactor: 重写 了controller decorator

---
+++ src/decorators/controller.ts
@@ -0,0 +1,11 @@
+import { normalizePath } from "src/utils";
+import { PATH_METADATA } from "../constants";
+
+export function Controller(): ClassDecorator;
+export function Controller(prefix: string): ClassDecorator;
+export function Controller(prefix: string = "/"): ClassDecorator {
+  return (target) => {
+    prefix = normalizePath(prefix);
+    Reflect.defineMetadata(PATH_METADATA, prefix, target);
+  };
+}
