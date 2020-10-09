e4df25465992f07311be2016d6d946729ada60fc
keke <keke@kekedeMacBook-Air.local>
2020-10-09 23:39:37 +0800

    feat:  增加了utils的测试

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,13 +1,13 @@
 import {
   normalizePath,
   isEmpty,
-  // isSymbol,
-  // isString,
-  // isUndefined,
-  // isConstructor,
-  // isObject,
-  // isPlainObject,
-  // isNil,
+  isSymbol,
+  isString,
+  isUndefined,
+  isConstructor,
+  isObject,
+  isPlainObject,
+  isNil,
 } from "../src/utils";
 
 describe("utils test", () => {
@@ -25,5 +25,51 @@ describe("utils test", () => {
 
   it("isEmpty", () => {
     expect(isEmpty([])).toBe(true);
+    expect(isEmpty(new Set())).toBe(false);
+    expect(isEmpty({})).toBe(false);
+    expect(isEmpty(undefined)).toBe(false);
+  });
+
+  it("isSymbol", () => {
+    const symbol1 = Symbol();
+    const symbol2 = Symbol("sym");
+    const symbol3 = Symbol;
+    const symbol4 = Symbol.for("sym");
+    expect(isSymbol(symbol1)).toBe(true);
+    expect(isSymbol(symbol2)).toBe(true);
+    expect(isSymbol(symbol3)).toBe(false);
+    expect(isSymbol(symbol4)).toBe(true);
+  });
+
+  it("isString", () => {
+    const str1 = "";
+    const str2 = "hello";
+    expect(isString(str1)).toBe(true);
+    expect(isString(str2)).toBe(true);
+  });
+
+  it("isUndefined", () => {
+    expect(isUndefined(undefined)).toBe(true);
+  });
+
+  it("isConstructor", () => {
+    expect(isConstructor("constructor")).toBe(true);
+  });
+
+  it("isObject", () => {
+    expect(isObject({})).toBe(true);
+    expect(isObject(null)).toBe(false);
+    expect(isObject([])).toBe(true);
+  });
+
+  it("isPlainObject", () => {
+    expect(isPlainObject({})).toBe(true);
+    expect(isPlainObject([])).toBe(false);
+  });
+
+  it("isNil", () => {
+    expect(isNil(null)).toBe(true);
+    expect(isNil(undefined)).toBe(true);
+    expect(isNil("")).toBe(false);
   });
 });
