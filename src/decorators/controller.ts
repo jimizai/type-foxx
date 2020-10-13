af1b247b972676c2e99056e354527e7536b47892
jimizai <359743984@qq.com>
2020-10-14 00:27:37 +0800

    lint: code style linted

--- src/decorators/controller.ts
+++ src/decorators/controller.ts
@@ -4,8 +4,8 @@ import { PATH_METADATA } from "../constants";
 export function Controller(): ClassDecorator;
 export function Controller(prefix: string): ClassDecorator;
 export function Controller(prefix: string = "/"): ClassDecorator {
-  return (target) => {
-    prefix = normalizePath(prefix);
-    Reflect.defineMetadata(PATH_METADATA, prefix, target);
-  };
+	return (target) => {
+		prefix = normalizePath(prefix);
+		Reflect.defineMetadata(PATH_METADATA, prefix, target);
+	};
 }
--- src/routes/base.ts
+++ src/routes/base.ts
@@ -2,53 +2,53 @@ import { controller, get } from "../decorators";
 
 @controller("/test")
 export class HelloController {
-  @get("/hello")
-  index(req: Foxx.Request, res: Foxx.Response) {
-    res.send({
-      code: 200,
-      data: [],
-      message: "success",
-      time: Date.now(),
-    });
-  }
+	@get("/hello")
+	index(req: Foxx.Request, res: Foxx.Response) {
+		res.send({
+			code: 200,
+			data: [],
+			message: "success",
+			time: Date.now()
+		});
+	}
 
-  @get("/list")
-  list(req: Foxx.Request, res: Foxx.Response) {
-    res.send({
-      code: 200,
-      data: [],
-      message: "success",
-      time: Date.now(),
-    });
-  }
+	@get("/list")
+	list(req: Foxx.Request, res: Foxx.Response) {
+		res.send({
+			code: 200,
+			data: [],
+			message: "success",
+			time: Date.now()
+		});
+	}
 
-  @get("/ttt")
-  ddd(req: Foxx.Request, res: Foxx.Response) {
-    res.send({
-      code: 200,
-      data: "ddd",
-      message: "success",
-      time: Date.now(),
-    });
-  }
+	@get("/ttt")
+	ddd(req: Foxx.Request, res: Foxx.Response) {
+		res.send({
+			code: 200,
+			data: "ddd",
+			message: "success",
+			time: Date.now()
+		});
+	}
 
-  @get("/ddd")
-  ttt(req: Foxx.Request, res: Foxx.Response) {
-    res.send({
-      code: 200,
-      data: "ddd",
-      message: "success",
-      time: Date.now(),
-    });
-  }
+	@get("/ddd")
+	ttt(req: Foxx.Request, res: Foxx.Response) {
+		res.send({
+			code: 200,
+			data: "ddd",
+			message: "success",
+			time: Date.now()
+		});
+	}
 
-  @get("/order")
-  order(req: Foxx.Request, res: Foxx.Response) {
-    res.send({
-      code: 200,
-      data: "ddd",
-      message: "success",
-      time: Date.now(),
-    });
-  }
+	@get("/order")
+	order(req: Foxx.Request, res: Foxx.Response) {
+		res.send({
+			code: 200,
+			data: "ddd",
+			message: "success",
+			time: Date.now()
+		});
+	}
 }
