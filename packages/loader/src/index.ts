e772fef79c1b8c51a313ce7d60009a1885927468
dyk <359743984@qq.com>
2021-04-27 22:34:03 +0800

    feat: add loader

---
+++ packages/loader/src/index.ts
@@ -0,0 +1,25 @@
+import glob from "glob";
+import * as path from "path";
+
+const FILE_PETTERNS = "**/*.?(ts|js)";
+
+export class Loader {
+  constructor(private srcDir: string) {}
+
+  load(): Promise<any[]> {
+    return new Promise((resolve) => {
+      const filePatterns = path.join(this.srcDir, FILE_PETTERNS);
+      glob(filePatterns, {}, (_, files) => {
+        console.log(files);
+        resolve(
+          files
+            .map((file) => {
+              const module = require(file);
+              return Object.keys(module).map((key) => module[key]);
+            })
+            .flat()
+        );
+      });
+    });
+  }
+}
