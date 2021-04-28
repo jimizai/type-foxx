f569281fb7263d4c433fdbb068f2793f47ad78c3
dyk <359743984@qq.com>
2021-04-28 20:37:36 +0800

    feat: add  decorator test

--- packages/decorators/__tests__/decorators.test.ts
+++ packages/decorators/__tests__/decorators.test.ts
@@ -1,10 +1,36 @@
-import { CLASS_SCOPE, Scope, ScopeEnum } from "../src";
+import {
+  CLASS_SCOPE,
+  Controller,
+  Get,
+  METHOD_METADATA,
+  PATH_METADATA,
+  RequestMethod,
+  Scope,
+  ScopeEnum,
+} from "../src";
 
 @Scope(ScopeEnum.Singleton)
+@Controller("/test")
 class A {
+  @Get("/api")
+  index() {
+  }
 }
 
 test("add scope", () => {
   const scope = Reflect.getMetadata(CLASS_SCOPE, A);
   expect(scope).toBe(ScopeEnum.Singleton);
+});
+
+test("add controller", () => {
+  const path = Reflect.getMetadata(PATH_METADATA, A);
+  expect(path).toBe("/test");
+});
+
+test("add request mappings", () => {
+  const a = new A();
+  const path = Reflect.getMetadata(PATH_METADATA, a.index);
+  const method = Reflect.getMetadata(METHOD_METADATA, a.index);
+  expect(path).toBe("/api");
+  expect(method).toBe(RequestMethod.GET);
 });
