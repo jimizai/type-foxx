7274b6bc49771d11f3db9cced5a5d962a12ac683
dyk <359743984@qq.com>
2021-05-06 13:41:32 +0800

    fix: routes init bug fixed

--- packages/core/src/routes.ts
+++ packages/core/src/routes.ts
@@ -1,20 +1,29 @@
-import { METHOD_METADATA, PATH_METADATA } from "@jimizai/decorators";
+import {
+  Arg,
+  METHOD_METADATA,
+  PARAM_METADATA,
+  PATH_METADATA,
+} from "@jimizai/decorators";
 import { FactoryContainer } from "@jimizai/injectable";
 import * as path from "path";
 
 interface Route {
   method: string;
   url: string;
-  args: string[];
+  args: Arg[];
 }
 
 export class RoutesContainer extends FactoryContainer {
-  protected routes: Route[];
+  protected routes: Route[] = [];
 
   //deno-lint-ignore no-explicit-any
   constructor(targets: any[]) {
     super(targets);
     this.initRoutes();
+  }
+
+  getRoutes() {
+    return this.routes;
   }
 
   initRoutes() {
@@ -24,18 +33,20 @@ export class RoutesContainer extends FactoryContainer {
       if (prefixPath) {
         const descriptors =
           (Object.getOwnPropertyDescriptors(module.target.prototype));
-        for (const methodName in Object.keys(descriptors)) {
+        for (const index in Object.keys(descriptors)) {
+          const methodName = Object.keys(descriptors)[index];
           if (methodName === "constructor") {
             continue;
           }
           const func = descriptors[methodName].value;
           const url = Reflect.getMetadata(PATH_METADATA, func);
           const method = Reflect.getMetadata(METHOD_METADATA, func);
+          const args = Reflect.getMetadata(PARAM_METADATA, func);
           if (url && method) {
             this.routes.push({
               method,
               url: path.join(prefixPath, url),
-              args: [],
+              args: args.sort((x, y) => x.parameterIndex - y.parameterIndex),
             });
           }
         }
