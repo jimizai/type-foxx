5ce5376f1382adcf543a227960b67b33dc6dc659
dyk <359743984@qq.com>
2021-05-06 21:21:24 +0800

    test: add koa driver tests

--- packages/drivers/__tests__/drivers.test.ts
+++ packages/drivers/__tests__/drivers.test.ts
@@ -1,2 +1,26 @@
-test("test", () => {
+import { Route } from "@jimizai/core";
+import { KoaDriver } from "../src";
+
+const routes: Route[] = [
+  {
+    method: "get",
+    url: "/test",
+    args: [],
+  },
+];
+
+test("test koa driver", () => {
+  const driver = new KoaDriver();
+  driver.use((ctx) => {
+    ctx.body = "hello";
+  });
+  expect(driver.middlewares.length).toBe(1);
+  driver.use((ctx) => {
+    ctx.body = "hello";
+  });
+  expect(driver.middlewares.length).toBe(2);
+
+  driver.useRoutes(routes);
+  expect(driver.routerInstance.stack.length).toBe(1);
+  expect(driver.middlewares.length).toBe(4);
 });
