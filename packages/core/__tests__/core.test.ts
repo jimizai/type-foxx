d74184085c71d1a7f1e6573791c7bed9fbb3a6f8
dyk <359743984@qq.com>
2021-05-06 13:41:45 +0800

    test: add core routes init tests

--- packages/core/__tests__/core.test.ts
+++ packages/core/__tests__/core.test.ts
@@ -1,2 +1,40 @@
-test("test", () => {
+import {
+  ArgType,
+  Controller,
+  Get,
+  Query,
+  RequestMethod,
+} from "@jimizai/decorators";
+import { Injectable } from "@jimizai/injectable";
+import { RoutesContainer } from "../src/routes";
+
+@Injectable()
+class B {
+  b = 12;
+
+  index() {
+    console.log("this is a b");
+  }
+}
+
+@Injectable()
+@Controller("/test")
+class A {
+  constructor(public b: B) {}
+  @Get("/ttt")
+  test(@Query("id") id: string) {
+    this.b.index();
+  }
+}
+
+test("bootstrap data init tests", () => {
+  const routesInstance = new RoutesContainer([A, B]);
+  const routes = routesInstance.getRoutes();
+  const route = routes[0];
+  expect(route.method).toBe(RequestMethod.GET);
+  expect(route.url).toBe("/test/ttt");
+  const arg = route.args[0];
+  expect(arg.parameterIndex).toBe(0);
+  expect(arg.name).toBe("id");
+  expect(arg.argType).toBe(ArgType.Query);
 });
