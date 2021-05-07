0112df7bcc7b76e7b545480b82fbc1d04f0f459d
dyk <359743984@qq.com>
2021-05-07 13:11:30 +0800

    fix: bug fixed

--- packages/drivers/src/koa/index.ts
+++ packages/drivers/src/koa/index.ts
@@ -1,4 +1,4 @@
-import { Route } from "@jimizai/core";
+import { RoutesContainer } from "@jimizai/core";
 import { ArgType, PARAM_ALL } from "@jimizai/decorators";
 import { Context, Middleware } from "koa";
 import ExtendContext from "./context";
@@ -14,12 +14,24 @@ const defaultDriverOptions: KoaDriverOptions = {
   port: 7001,
 };
 
+type ExtendInterface<T> = {
+  [P in keyof T]: T[P];
+};
+
+interface FoxxContext extends Context, ExtendInterface<typeof ExtendContext> {
+  //deno-lint-ignore no-explicit-any
+  requestContext: { get(ident: string): any };
+}
+
 export class KoaDriver {
   public instance: typeof Koa;
   public routerInstance: typeof Router;
   public middlewares: Middleware[] = [];
 
-  constructor(private options: KoaDriverOptions = defaultDriverOptions) {
+  constructor(
+    private routesContainer: RoutesContainer,
+    private options: KoaDriverOptions = defaultDriverOptions,
+  ) {
     this.instance = new Koa();
     this.routerInstance = new Router();
   }
@@ -29,25 +41,46 @@ export class KoaDriver {
     return this;
   }
 
-  useRoutes(routes: Route[]) {
+  private makeExtendContext(
+    routesContainer: RoutesContainer,
+  ): (ctx: Context) => FoxxContext {
+    return (ctx) => {
+      ctx.requestContext = {
+        get(identity: string) {
+          return routesContainer.get(identity);
+        },
+      };
+      return Object.assign(ctx, ExtendContext) as FoxxContext;
+    };
+  }
+
+  public useRoutes() {
+    const routes = this.routesContainer.getRoutes();
+    const extendContext = this.makeExtendContext(this.routesContainer);
     routes.forEach((route) => {
-      this.routerInstance[route.method](route.url, (ctx: Context) => {
-        ctx = Object.assign(ctx, ExtendContext);
-        const args = route.args.map((arg) => {
-          const target = arg.argType === ArgType.Query
-            ? ctx.query
-            : arg.argType === ArgType.Body
-            ? // deno-lint-ignore no-explicit-any
-              (ctx.request as any).body
-            : ctx.params;
-          if (arg.name === PARAM_ALL) {
-            return target;
-          } else {
-            return target[arg.name];
-          }
-        });
-        route.func.apply(route.target, args);
-      });
+      this.routerInstance[route.method](
+        route.url,
+        async (koaCtx: Context, next) => {
+          const ctx = extendContext(koaCtx);
+          const args = route.args.map((arg) => {
+            const target = arg.argType === ArgType.Query
+              ? ctx.query
+              : arg.argType === ArgType.Body
+              ? // deno-lint-ignore no-explicit-any
+                (ctx.request as any).body
+              : ctx.params;
+            if (arg.name === PARAM_ALL) {
+              return target;
+            } else {
+              return target[arg.name];
+            }
+          });
+          const controller = ctx.requestContext.get(route.identity);
+          const func = controller[route.funcName];
+          await func?.apply?.(controller, args);
+          await next();
+        },
+      );
     });
     this.use(this.routerInstance.routes()).use(
       this.routerInstance.allowedMethods(),
@@ -55,9 +88,9 @@ export class KoaDriver {
     return this;
   }
 
-  listen() {
+  public init() {
     this.middlewares.forEach((middleware) => this.instance.use(middleware));
-
+    this.useRoutes();
     this.instance.listen(this.options.port, () => {
       console.log("server started on http://localhost:" + this.options.port);
     });
