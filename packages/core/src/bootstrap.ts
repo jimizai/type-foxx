f3bb08e00c5dd69fcfe7d93cf31f6f1f6f4aad18
dyk <359743984@qq.com>
2021-05-09 14:51:50 +0800

    feat: support modules

--- packages/core/src/bootstrap.ts
+++ packages/core/src/bootstrap.ts
@@ -1,3 +1,4 @@
+import { MODULE_METADATA, ModuleOptions } from "@jimizai/decorators";
 import { FoxxDriver, KoaFoxxDriver } from "@jimizai/drivers";
 import { CLASS_METADATA } from "@jimizai/injectable";
 import { Loader } from "@jimizai/loader";
@@ -8,18 +9,30 @@ import ora = require("ora");
 interface BootstrapOptions<Middleware> {
   Driver?: FoxxDriver<Middleware>;
   port?: number;
-  srcDir?: string;
+  srcDirs?: string[] | string;
   middlewares?: Middleware[];
+  // deno-lint-ignore ban-types
+  modules?: Function[];
 }
+
+const toArray = <T>(arr: T | T[]): T[] => Array.isArray(arr) ? arr : [arr];
 
 //deno-lint-ignore no-explicit-any
 export async function boostrap<Middleware = any>(
   options: BootstrapOptions<Middleware> = {},
 ) {
   const spinner = ora("Foxx server starting..").start();
-  const srcDir = options.srcDir || path.join(process.cwd(), "./src");
-  const modules = await new Loader(srcDir).load();
-  const injectableModules = modules.filter((module) =>
+  const dirs = toArray(options.srcDirs);
+  const moduleDirs = (options.modules || []).map((target) =>
+    (Reflect.getMetadata(MODULE_METADATA, target) as ModuleOptions)?.srcDir ||
+    ""
+  ).filter(Boolean);
+  let srcDirs: string[] = [...dirs, ...moduleDirs];
+  if (!srcDirs.length) {
+    srcDirs = [path.join(process.cwd(), "./src")];
+  }
+  const m = await new Loader(srcDirs).load();
+  const injectableModules = m.filter((module) =>
     Reflect.getMetadata(CLASS_METADATA, module)
   );
   const routesInstance = new RoutesContainer(injectableModules);
