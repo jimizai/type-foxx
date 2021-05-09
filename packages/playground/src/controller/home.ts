3015111125a0d8d62c3b602b59eac9fd610111db
dyk <359743984@qq.com>
2021-05-09 16:24:02 +0800

    rm: drop console

--- packages/playground/src/controller/home.ts
+++ packages/playground/src/controller/home.ts
@@ -1,23 +1,19 @@
 import { Controller, Get } from "../../../decorators";
 import { Injectable } from "../../../injectable";
 import { DataService } from "../../services/data";
-import { TestService } from "../services/TestService";
 
 @Injectable()
 @Controller()
 export class HomeController {
   constructor(
-    private testService: TestService,
     private dataService: DataService,
   ) {}
 
   @Get()
   home() {
-    console.log(this.testService);
-    console.log(this.dataService);
     return {
       code: 200,
-      data: this.testService.add(1, 1),
+      data: this.dataService.getData(),
       msg: "success",
       timestamp: Date.now(),
     };
