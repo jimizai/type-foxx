c4d38fbb74ec379cedf81c26b5b9798ed28c114d
dyk <359743984@qq.com>
2021-05-09 20:33:20 +0800

    feat: add ctx decorator

--- packages/playground/src/controller/home.ts
+++ packages/playground/src/controller/home.ts
@@ -1,19 +1,28 @@
-import { Controller, Get } from "../../../decorators";
+import { Context } from "koa";
+import { Controller, Ctx, Get } from "../../../decorators";
 import { Injectable } from "../../../injectable";
 import { DataService } from "../../services/data";
 
 @Injectable()
 @Controller()
 export class HomeController {
-  constructor(
-    private dataService: DataService,
-  ) {}
+  constructor(private dataService: DataService) {}
 
   @Get()
   home() {
     return {
       code: 200,
       data: this.dataService.getData(),
+      msg: "success",
+      timestamp: Date.now(),
+    };
+  }
+
+  @Get("/ttt")
+  test(@Ctx() ctx: Context) {
+    ctx.body = {
+      code: 200,
+      data: "hello test",
       msg: "success",
       timestamp: Date.now(),
     };
