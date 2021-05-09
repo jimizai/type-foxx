57c5bdc0c69d663cd15767e9037a0f8474b5f4af
dyk <359743984@qq.com>
2021-05-09 19:22:19 +0800

    feat: add req res ctx decorators

--- packages/decorators/src/arg-mappings.ts
+++ packages/decorators/src/arg-mappings.ts
@@ -1,9 +1,12 @@
-import { PARAM_ALL, PARAM_METADATA } from "./constants";
+import { PARAM_METADATA } from "./constants";
 
 export enum ArgType {
   Query = "query",
   Param = "param",
   Body = "body",
+  Req = "req",
+  Res = "res",
+  Ctx = "ctx",
 }
 
 export interface Arg {
@@ -15,9 +18,8 @@ export interface Arg {
 export const makeArgMappings = (
   argType: ArgType,
 ) =>
-  (argName = PARAM_ALL): ParameterDecorator =>
-    //deno-lint-ignore no-explicit-any
-    (target: any, propertyKey: string | symbol, parameterIndex: number) => {
+  (argName?: string): ParameterDecorator =>
+    (target, propertyKey: string | symbol, parameterIndex: number) => {
       const method = target[propertyKey];
       const args: Arg[] = Reflect.getMetadata(PARAM_METADATA, method) || [];
       args.push({
@@ -31,3 +33,7 @@ export const makeArgMappings = (
 export const Query = makeArgMappings(ArgType.Query);
 export const Param = makeArgMappings(ArgType.Param);
 export const Body = makeArgMappings(ArgType.Body);
+export const Request = makeArgMappings(ArgType.Req);
+export const Req = Request;
+export const Response = makeArgMappings(ArgType.Res);
+export const Res = Response;
