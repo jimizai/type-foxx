858e99eb534b149a23893b5bd41fe57939bd2294
dyk <359743984@qq.com>
2021-05-09 14:22:14 +0800

    feat: loader support dirs

--- packages/loader/src/index.ts
+++ packages/loader/src/index.ts
@@ -3,23 +3,40 @@ const glob = require("glob");
 
 const FILE_PETTERNS = "**/*.?(ts|js)";
 
+const globSync = (url: string) =>
+  new Promise((resolve, reject) => {
+    glob(path.join(url, FILE_PETTERNS), {}, (err, files) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(
+        files,
+      );
+    });
+  });
+
 export class Loader {
-  constructor(private srcDir: string) {}
+  constructor(private srcDir: string[] | string) {}
+
+  getSrcDirs(): string[] {
+    return Array.isArray(this.srcDir) ? this.srcDir : [this.srcDir];
+  }
+
+  async getFiles(): Promise<string[]> {
+    const files =
+      (await Promise.all(this.getSrcDirs().map((src) => globSync(src))))
+        .flat() as string[];
+    return [...new Set(files)];
+  }
 
   // deno-lint-ignore no-explicit-any
-  load(): Promise<any[]> {
-    return new Promise((resolve) => {
-      const filePatterns = path.join(this.srcDir, FILE_PETTERNS);
-      glob(filePatterns, {}, (_, files) => {
-        resolve(
-          files
-            .map((file) => {
-              const module = require(file);
-              return Object.keys(module).map((key) => module[key]);
-            })
-            .flat(),
-        );
-      });
-    });
+  async load(): Promise<any[]> {
+    const files = await this.getFiles();
+    return files
+      .map((file) => {
+        const module = require(file);
+        return Object.keys(module).map((key) => module[key]);
+      })
+      .flat();
   }
 }
