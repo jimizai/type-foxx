c9f55a9dc53db85a3e11d0d1e6b438cce70749fc
dyk <359743984@qq.com>
2021-05-09 14:21:36 +0800

    test: add loader tests

--- packages/loader/__tests__/loader.test.ts
+++ packages/loader/__tests__/loader.test.ts
@@ -1,16 +1,28 @@
-import { Loader } from "../src";
 import * as path from "path";
-import { A } from "./A";
-import { B } from "./B";
-import C from "./C";
-import { D } from "./D";
-import { E } from "./E";
+import { Loader } from "../src";
+import { F } from "./modules/F";
+import { A } from "./services/A";
+import { B } from "./services/B";
+import C from "./services/C";
+import { D } from "./services/D";
+import { E } from "./services/E";
 
 test("can load files", async () => {
-  const targets = await new Loader(path.resolve(__dirname, "./")).load();
+  const targets = await new Loader(path.resolve(__dirname, "./modules"))
+    .load();
+  expect(targets[0]).toBe(F);
+});
+
+test("support srcDirs", async () => {
+  const targets = await new Loader([
+    path.resolve(__dirname, "./services"),
+    path.resolve(__dirname, "./modules"),
+  ])
+    .load();
   expect(targets[0]).toBe(A);
   expect(targets[1]).toBe(B);
   expect(targets[2]).toBe(C);
   expect(targets[3]).toBe(D);
   expect(targets[4]).toBe(E);
+  expect(targets[5]).toBe(F);
 });
