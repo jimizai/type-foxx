93f1457560d1e2cadfcb6f5f4930a479d8a5e268
dyk <359743984@qq.com>
2021-05-11 22:52:24 +0800

    fix: change config module

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,7 +1,21 @@
-import { Module } from "@jimizai/decorators";
+// import { DynamicModule } from "@jimizai/core";
 import { resolve } from "path";
+import { FOXX_CONFIGS } from "./constants";
 
 export * from "./service";
+export { FOXX_CONFIGS };
 
-@Module({ srcDir: resolve(__dirname, "./") })
-export class ConfigModule {}
+export class ConfigModule {
+  //deno-lint-ignore no-explicit-any
+  static register(configs: any): any {
+    return {
+      srcDir: resolve(__dirname, "./"),
+      providers: [
+        {
+          provide: FOXX_CONFIGS,
+          useValues: configs,
+        },
+      ],
+    };
+  }
+}
