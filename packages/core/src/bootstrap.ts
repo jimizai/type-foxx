ea47b9f3bf142c723d732006ece83c54f60e7d5a
dyk <359743984@qq.com>
2021-05-11 21:33:30 +0800

    fix: bootstrap bug fixed

--- packages/core/src/bootstrap.ts
+++ packages/core/src/bootstrap.ts
@@ -16,33 +16,48 @@ interface BootstrapOptions<Middleware> {
 }
 
 const toArray = <T>(arr: T | T[]): T[] => Array.isArray(arr) ? arr : [arr];
+const isFunction = (func: unknown): boolean => typeof func === "function";
 
 //deno-lint-ignore no-explicit-any
 export async function boostrap<Middleware = any>(
   options: BootstrapOptions<Middleware> = {},
 ) {
   const spinner = ora("Foxx server starting..").start();
-  const dirs = toArray(options.srcDirs);
-  const moduleDirs = (options.modules || []).map((target) =>
-    (Reflect.getMetadata(MODULE_METADATA, target) as ModuleOptions)?.srcDir ||
-    ""
-  );
-  let srcDirs: string[] = [...dirs, ...moduleDirs].filter(Boolean);
-  if (!srcDirs.length) {
-    srcDirs = [path.join(process.cwd(), "./src")];
+  try {
+    const dirs = toArray(options.srcDirs);
+    const moduleDirs = (options.modules || []).map((target) =>
+      (Reflect.getMetadata(MODULE_METADATA, target) as ModuleOptions)?.srcDir ||
+      ""
+    );
+    let srcDirs: string[] = [...dirs, ...moduleDirs].filter(Boolean);
+    if (!srcDirs.length) {
+      srcDirs = [path.join(process.cwd(), "./src")];
+    }
+    const m = await new Loader(srcDirs).load();
+    const injectableModulesObject = (m.filter((module) =>
+      isFunction(module) && Reflect.getMetadata(CLASS_METADATA, module)
+    ).reduce(
+      (prev, target) => ({ ...prev, [target.name]: target }),
+      //deno-lint-ignore ban-types
+      {} as Record<string, Function>,
+    ));
+
+    const injectableModules = Object.keys(injectableModulesObject).map((key) =>
+      injectableModulesObject[key]
+    );
+    const routesInstance = new RoutesContainer(injectableModules);
+    //deno-lint-ignore no-explicit-any
+    const Driver: any = options.Driver || KoaFoxxDriver;
+    const driver: FoxxDriver<Middleware> = new Driver(routesInstance, {
+      port: options.port || 7001,
+    });
+    driver.useMiddlewares(options.middlewares || []);
+    spinner.text = "Foxx server started success!";
+    spinner.succeed();
+    driver.init();
+  } catch (err) {
+    console.log(err);
+    spinner.text = "Foxx server started error!";
+    spinner.fail();
   }
-  const m = await new Loader(srcDirs).load();
-  const injectableModules = m.filter((module) =>
-    Reflect.getMetadata(CLASS_METADATA, module)
-  );
-  const routesInstance = new RoutesContainer(injectableModules);
-  //deno-lint-ignore no-explicit-any
-  const Driver: any = options.Driver || KoaFoxxDriver;
-  const driver: FoxxDriver<Middleware> = new Driver(routesInstance, {
-    port: options.port || 7001,
-  });
-  driver.useMiddlewares(options.middlewares || []);
-  spinner.text = "Foxx server started success!";
-  spinner.succeed();
-  driver.init();
 }
