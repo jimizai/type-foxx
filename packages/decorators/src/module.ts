dfa957701945d70df448d8068a565c4c8779cad9
dyk <359743984@qq.com>
2021-05-11 21:45:37 +0800

    feat: add options providers

--- packages/decorators/src/module.ts
+++ packages/decorators/src/module.ts
@@ -1,7 +1,14 @@
 import { MODULE_METADATA } from "./constants";
 
+//deno-lint-ignore no-explicit-any
+export type Provider = (...args: any[]) => ({
+  provide: string;
+  //deno-lint-ignore no-explicit-any
+  useValues: any;
+});
 export interface ModuleOptions {
   srcDir?: string;
+  providers?: Provider[];
 }
 
 export function Module(options?: ModuleOptions): ClassDecorator {
