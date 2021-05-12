a43f948761211887fcec883d9ecb1e8a30af6537
dyk <359743984@qq.com>
2021-05-12 15:35:42 +0800

    fix: remove module options

--- packages/decorators/src/module.ts
+++ packages/decorators/src/module.ts
@@ -3,20 +3,16 @@ import { MODULE_METADATA } from "./constants";
 export interface DynamicModule {
   srcDir?: string;
   providers?: Provider[];
+  //deno-lint-ignore ban-types
+  modules: (Function | DynamicModule)[];
 }
 export interface Provider {
   provide: string;
   //deno-lint-ignore no-explicit-any
   useValues: any;
 }
-export interface ModuleOptions {
-  srcDir?: string;
-  providers?: Provider[];
-  //deno-lint-ignore ban-types
-  modules?: (Function | DynamicModule)[];
-}
 
-export function Module(options?: ModuleOptions): ClassDecorator {
+export function Module(options?: DynamicModule): ClassDecorator {
   return (target) => {
     Reflect.defineMetadata(MODULE_METADATA, options || {}, target);
   };
