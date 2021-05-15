2e9513f2c406a8a8b1a11cfaab900c4183260906
dyk <359743984@qq.com>
2021-05-15 17:40:58 +0800

    init: add driver types

---
+++ packages/driver-types/README.md
@@ -0,0 +1,11 @@
+# `@jimizai/driver-types`
+
+> TODO: description
+
+## Usage
+
+```
+const driverTypes = require('@jimizai/driver-types');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/driver-types/src/index.ts
@@ -0,0 +1,10 @@
+export interface FoxxDriverOptions {
+  port?: number;
+}
+
+export interface FoxxDriver<Middleware> {
+  init(): void;
+  useRoutes(): this;
+  useMiddlewares(middlewares: Middleware[]): this;
+  use(middleware: Middleware): this;
+}
