b01e0e3596e0b45ed9043d739cd5d1777b68041e
dyk <359743984@qq.com>
2021-05-16 17:54:25 +0800

    feat: add test

--- packages/common/__tests__/common.test.ts
+++ packages/common/__tests__/common.test.ts
@@ -1,1 +1,10 @@
-test("test common", () => {});
+import { BadRequestException, BaseExceptions } from "../src";
+
+test("test exceptions", () => {
+  try {
+    throw new BadRequestException();
+  } catch (error) {
+    expect(error instanceof BaseExceptions).toBe(true);
+    expect(error instanceof Error).toBe(true);
+  }
+});
