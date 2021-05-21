b25efbb36f2432ea133a9f84a3e1ec863e806f4a
dyk <359743984@qq.com>
2021-05-21 12:53:47 +0800

    feat: add factory container interface

--- packages/driver-types/src/index.ts
+++ packages/driver-types/src/index.ts
@@ -8,3 +8,27 @@ export interface FoxxDriver<Middleware> {
   useMiddlewares(middlewares: Middleware[]): this;
   use(middleware: Middleware): this;
 }
+
+enum ArgType {
+  Query = "query",
+  Param = "param",
+  Body = "body",
+  Req = "req",
+  Res = "res",
+  Ctx = "ctx",
+}
+
+export interface Route {
+  method: string;
+  url: string;
+  funcName: string;
+  identity: string;
+  args: { name: string; argType: ArgType; parameterIndex: number }[];
+}
+
+export interface FoxxFactoryInterface {
+  get<T>(identity: string): T;
+  getRoutes(): Route[];
+  // deno-lint-ignore no-explicit-any
+  getHandlers(): { instance: any; handlers: any[] }[];
+}
