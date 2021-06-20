cfe91cfd82010bcc69c77b0ae306346ae86e034d
dyk <359743984@qq.com>
2021-06-20 10:24:20 +0800

    rm: drop foxx

--- packages/driver-foxx/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./driver";
-import { Context } from "./context";
-
-//deno-lint-ignore no-explicit-any
-export interface ExceptionFilter<T = any> {
-  catch(exception: T, ctx: Context): void;
-}
