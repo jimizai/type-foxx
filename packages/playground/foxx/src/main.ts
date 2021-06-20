9dafe3e21996054c1f11894e8a73bcfe89717436
dyk <359743984@qq.com>
2021-06-20 10:24:20 +0800

    rm: drop foxx

--- packages/playground/foxx/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { boostrap } from "@jimizai/core";
-import { TypeFoxxDriver } from "@jimizai/driver-foxx";
-import * as path from "path";
-
-boostrap({
-  Driver: TypeFoxxDriver,
-  srcDirs: [path.resolve(__dirname, "./")],
-});
