466cd5d1af9b55a974ae110ea54d7ff8f475533b
dyk <359743984@qq.com>
2021-08-10 23:33:37 +0800

    feat: add utils

---
+++ libs/utils/src/lib/utils.ts
@@ -0,0 +1,27 @@
+export const isUndefined = (val: unknown): val is undefined =>
+  typeof val === 'undefined';
+
+export const isNull = (val: unknown): val is null => val === null;
+
+export const isNil = (val: unknown): val is null | undefined =>
+  isUndefined(val) || isNull(val);
+
+export const isNotNil = (val: unknown): boolean => !isNil(val);
+
+export const sleep = (time) =>
+  new Promise((resolve) => setTimeout(resolve, time));
+
+// eslint-disable-next-line
+export const isNotEmpty = (val: Record<string, any> | Array<any>): boolean => {
+  if (!val) {
+    return false;
+  }
+  if (Array.isArray(val)) {
+    return !!val.length;
+  }
+  return JSON.stringify(val) !== '{}';
+};
+
+// eslint-disable-next-line
+export const isEmpty = (val: Record<string, any> | Array<any>): boolean =>
+  !isNotEmpty(val);
