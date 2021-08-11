7e0814c72e09eba903e9e19df5db78379635fc6c
dyk <359743984@qq.com>
2021-08-11 20:33:30 +0800

    fix: bug fixed

--- libs/injectable/src/lib/container.ts
+++ libs/injectable/src/lib/container.ts
@@ -3,7 +3,7 @@ import {
   TARGET_INJECTABLE,
   ROOT_TARGET_INJECTABLE,
 } from './constants';
-import { isNotEmpty } from '@jimizai/utils';
+import { isUndefined, isNil } from '@jimizai/utils';
 
 export class Container {
   static targets: { [key: string]: any } = {};
@@ -13,33 +13,49 @@ export class Container {
   }
 
   static get(key: string) {
-    return this.targets[key] || {};
+    return this.targets[key];
   }
 
-  static factory = <T>(c: any): T => {
+  static factory = <T>(c: { new (...args: any[]): T }): T => {
+    if (Reflect.getMetadata(ROOT_TARGET_INJECTABLE, c)) {
+      return Reflect.getMetadata(ROOT_TARGET_INJECTABLE, c);
+    }
+
+    // args replaced
     const args = Reflect.getMetadata('design:paramtypes', c);
     const replaceArgs = Reflect.getMetadata(INJECT_ARG_INDEX, c) || [];
-    replaceArgs.forEach(({ index, identifier }) => {
-      args[index] = Container.get(identifier) || {};
+    replaceArgs.forEach(({ index, identifier, def }) => {
+      if (!isUndefined(Container.get(identifier))) {
+        args[index] = Container.get(identifier);
+      } else if (!isUndefined(def)) {
+        args[index] = def;
+      }
     });
-    return new c(
+
+    // factory
+    const target = new c(
       ...(args?.map((arg) => {
-        if (!(typeof arg === 'object' || typeof arg === 'function')) {
+        if (isNil(arg)) {
+          return arg;
+        }
+        if (typeof arg !== 'object' && typeof arg !== 'function') {
           return arg;
         }
         const data = Reflect.getMetadata(TARGET_INJECTABLE, arg);
-        if (isNotEmpty(Reflect.getMetadata(ROOT_TARGET_INJECTABLE, arg))) {
-          return Reflect.getMetadata(ROOT_TARGET_INJECTABLE, arg);
-        }
         let result = arg;
         if (data) {
           result = Container.factory(arg);
-          if (data.providedIn === 'root') {
-            Reflect.defineMetadata(ROOT_TARGET_INJECTABLE, result, arg);
-          }
         }
         return result;
       }) || [])
     );
+
+    // impl provideIn: 'root'
+    const data = Reflect.getMetadata(TARGET_INJECTABLE, c);
+    if (data && data.providedIn === 'root') {
+      Reflect.defineMetadata(ROOT_TARGET_INJECTABLE, target, c);
+    }
+
+    return target;
   };
 }
