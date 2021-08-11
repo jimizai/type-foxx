13f5e34362772a22678cedddac1d3e83d69ab193
dyk <359743984@qq.com>
2021-08-11 20:38:41 +0800

    feat: add impl inject def

--- libs/injectable/src/lib/decorators.ts
+++ libs/injectable/src/lib/decorators.ts
@@ -1,4 +1,5 @@
 import { TARGET_INJECTABLE, INJECT_ARG_INDEX } from './constants';
+import { isUndefined } from '@jimizai/utils';
 import { Container } from './container';
 
 export function Injectable(args: { providedIn?: 'root' } = {}): ClassDecorator {
@@ -7,15 +8,17 @@ export function Injectable(args: { providedIn?: 'root' } = {}): ClassDecorator {
   };
 }
 
-export function Inject(identifier: string): ParameterDecorator {
+export function Inject<T>(identifier: string, def?: T): ParameterDecorator {
   return (target, _propertyKey: string | symbol, parameterIndex: number) => {
     const args = Reflect.getMetadata(INJECT_ARG_INDEX, target) || [];
-    args.push({ index: parameterIndex, identifier });
-    const AutoHandleError = () => {
-      throw new Error('请先在Container中绑定' + identifier);
-    };
-    Injectable()(AutoHandleError);
-    Container.bind(identifier, AutoHandleError);
+    args.push({ index: parameterIndex, identifier, def });
+    if (isUndefined(def)) {
+      const AutoHandleError = () => {
+        throw new Error('请先在Container中绑定' + identifier);
+      };
+      Injectable()(AutoHandleError);
+      Container.bind(identifier, AutoHandleError);
+    }
     Reflect.defineMetadata(INJECT_ARG_INDEX, args, target);
   };
 }
