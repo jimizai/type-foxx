177b2297d40cabed217aa92b460bdf2fa293146c
dyk <359743984@qq.com>
2021-08-11 22:59:31 +0800

    test: add tests

--- libs/driver-koa/src/lib/driver-koa.spec.ts
+++ libs/driver-koa/src/lib/driver-koa.spec.ts
@@ -1,7 +1,12 @@
-import { driverKoa } from './driver-koa';
+import { KoaFoxxDriver } from './driver-koa';
+import { Container } from '@jimizai/injectable';
 
 describe('driverKoa', () => {
   it('should work', () => {
-    expect(driverKoa()).toEqual('driver-koa');
+    const instance: any = Container.factory(KoaFoxxDriver);
+    console.log(instance);
+    expect(instance.port).toBe(7001);
+    expect(instance.middlewares.toString()).toBe('');
+    expect(instance.routes.toString()).toBe('');
   });
 });
