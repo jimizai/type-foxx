6fbff4ad2471c4b89cfd22c6579dd80bba17e532
dyk <359743984@qq.com>
2021-08-11 21:17:54 +0800

    test: add tests

--- libs/injectable/src/lib/injectable.spec.ts
+++ libs/injectable/src/lib/injectable.spec.ts
@@ -1,7 +1,23 @@
 import 'reflect-metadata';
-import { Injectable } from './decorators';
+import { Inject, Injectable } from './decorators';
 import { Container } from './container';
 import { ROOT_TARGET_INJECTABLE } from './constants';
+
+const INEJCT_ARG = 'INEJCT_ARG';
+const DEFAULT_ARG = 'hello wolrd';
+const INEJCT_ARG2 = 'INEJCT_ARG2';
+
+@Injectable()
+class A {
+  constructor(@Inject(INEJCT_ARG, 'hello wolrd') public str: string) {}
+}
+
+@Injectable()
+class B {
+  constructor(@Inject(INEJCT_ARG2) public str: string) {
+    //
+  }
+}
 
 @Injectable({ providedIn: 'root' })
 class D {}
@@ -20,11 +36,20 @@ class E {
 }
 
 describe('injectable', () => {
-  it('should work', () => {
+  it('should auto injectable work', () => {
     const c = Container.factory(C);
     const root_c = Reflect.getMetadata(ROOT_TARGET_INJECTABLE, C);
     expect(root_c).toBe(c);
     const e = Container.factory(E);
     expect(e.d).toBe(c.d);
+  });
+
+  it('should inject default value work', () => {
+    const a = Container.factory(A);
+    expect(a.str).toBe('hello wolrd');
+
+    Container.bind(INEJCT_ARG2, DEFAULT_ARG);
+    const b = Container.factory(B);
+    expect(b.str).toBe('hello wolrd');
   });
 });
