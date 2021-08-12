bd80d5c0c70a4db64e4094423def4f290420cc96
dyk <359743984@qq.com>
2021-08-12 19:17:23 +0800

    feat: controller decorator impl injectable

--- libs/decorators/src/lib/controller.ts
+++ libs/decorators/src/lib/controller.ts
@@ -1,10 +1,12 @@
 import { PATH_METADATA } from './constants';
 import { normalizePath } from '@jimizai/utils';
+import { Injectable } from '@jimizai/injectable';
 
 export function Controller(): ClassDecorator;
 export function Controller(prefix: string): ClassDecorator;
 export function Controller(prefix = '/'): ClassDecorator {
   return (target) => {
+    Injectable()(target);
     prefix = normalizePath(prefix);
     Reflect.defineMetadata(PATH_METADATA, prefix, target);
   };
