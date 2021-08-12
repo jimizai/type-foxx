82ccf25800fc29b5ab4d4e75f63f3436c11ef800
dyk <359743984@qq.com>
2021-08-12 22:06:52 +0800

    chore: add home service injectable

--- apps/simple/src/app/home.controller.ts
+++ apps/simple/src/app/home.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@jimizai/decorators';
+import { HomeService } from './home.service';
 
 @Controller()
 export class HomeContoller {
+  constructor(private homeService: HomeService) {}
+
   @Get()
   async home() {
     return {
       code: 200,
-      data: 'success',
+      data: this.homeService.getData(),
     };
   }
 
