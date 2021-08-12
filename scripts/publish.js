899edab4a18e461e4660c1f567eb094ef620d00b
dyk <359743984@qq.com>
2021-08-12 22:27:06 +0800

    feat: add publish scripts

---
+++ scripts/publish.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env zx
+const path = require('path');
+const libPath = path.resolve(__dirname, '../libs');
+const distPath = path.join(__dirname, '../dist/libs');
+
+(async () => {
+  const BASE_ORIGIN = '2.x';
+  const BASE_ORIGIN = 'master';
+
+  const versionArgs = ['patch', 'minor', 'major'];
+  let type = process.argv.pop().toLowerCase();
+  if (!versionArgs.includes(type)) {
+    type = 'patch';
+  }
+
+  const result =
+    await $`nx affected:libs --base=${BASE_ORIGIN} --exclude simple --plain`;
+  const stdout = result.stdout?.replace(/[\r\n]/g, '') || '';
+  const affected = stdout?.split(' ').filter(Boolean);
+  if (!affected.length) {
+    return;
+  }
+
+  const oldPath = process.cwd();
+
+  for (let libName of affected) {
+    const url = path.resolve(libPath, libName);
+    process.chdir(url);
+    await $`npm version ${type}`;
+  }
+
+  process.chdir(oldPath);
+  await $`nx affected:build --with-deps --prod --parallel --exclude simple`;
+
+  for (let libName of affected) {
+    const distUrl = path.resolve(distPath, libName);
+    process.chdir(distUrl);
+    await $`npm publish`;
+  }
+  process.chdir(oldPath);
+
+  await $`git add .`;
+  await $`git commit -m "Publish"`;
+  await $`git push origin ${BASE_ORIGIN}`;
+  await $`git checkout ${BASE_ORIGIN}`;
+  await $`git merge ${BASE_ORIGIN}`;
+  await $`git push origin ${BASE_ORIGIN}`;
+  await $`git checkout ${BASE_ORIGIN}`;
+})();
