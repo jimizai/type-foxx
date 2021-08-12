54543883c84e67a6369cd55d4ab0a0c4cda7c0c1
dyk <359743984@qq.com>
2021-08-12 19:46:21 +0800

    fix: bug fixed

--- libs/core/src/lib/factory.ts
+++ libs/core/src/lib/factory.ts
@@ -1,21 +1,13 @@
-import { Injectable, Inject, Container } from '@jimizai/injectable';
-import { INJECT_FOXX_DRIVER, INJECT_ROUTES } from '@jimizai/driver-types';
+import { Injectable, Container } from '@jimizai/injectable';
+import { INJECT_ROUTES } from '@jimizai/driver-types';
 import { RoutesFactoryContainer } from './routes';
-import { FoxxDriver } from '@jimizai/driver-types';
 
 @Injectable()
 export class FoxxFactory {
-  constructor(
-    @Inject(INJECT_FOXX_DRIVER) private driver: FoxxDriver,
-    private routesFactoryContainer: RoutesFactoryContainer
-  ) {}
+  constructor(private routesFactoryContainer: RoutesFactoryContainer) {}
 
   async initRoutes() {
     const routes = await this.routesFactoryContainer.initRoutes();
     Container.bind(INJECT_ROUTES, routes);
-  }
-
-  bootstrap() {
-    this.driver.bootstrap();
   }
 }
