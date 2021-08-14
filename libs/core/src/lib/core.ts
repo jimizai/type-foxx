a32bed54eae6a9e595ee3f2a2ded54510cd43ce4
dyk <359743984@qq.com>
2021-08-14 21:23:56 +0800

    feat: add auto import drivers function

--- libs/core/src/lib/core.ts
+++ libs/core/src/lib/core.ts
@@ -11,19 +11,43 @@ import * as path from 'path';
 
 export type FoxxDriverConstructorTypeOf<T> = new (...args: any[]) => T;
 interface BootstrapOptions<Middleware> {
-  Driver: FoxxDriverConstructorTypeOf<FoxxDriver>;
+  Driver?: FoxxDriverConstructorTypeOf<FoxxDriver>;
   port?: number;
   srcDirs?: string[] | string;
   middlewares?: Middleware[];
 }
 
+const driverList = ['@jimizai/driver-koa'];
+
+const tryRequireDriver = async () => {
+  for (const driver of driverList) {
+    try {
+      const module = await import(driver);
+      console.log(module);
+      return module[Object.keys(module)[0]];
+    } catch (err) {
+      console.log(err);
+      continue;
+    }
+  }
+  return null;
+};
+
 export async function boostrap<M>(options: BootstrapOptions<M>) {
-  if (!options.Driver) {
-    throw new Error('Driver not found! Maybe you should install a driver');
+  let Driver;
+  if (options.Driver) {
+    Driver = options.Driver;
+  } else {
+    Driver = await tryRequireDriver();
+  }
+  if (!Driver) {
+    throw new Error(
+      'Driver not found! Maybe you should install @jimizai/driver-koa'
+    );
   }
   try {
     Container.bind(INJECT_FOXX_MIDDLEWARES, options.middlewares);
-    Container.bind(INJECT_FOXX_DRIVER, options.Driver);
+    Container.bind(INJECT_FOXX_DRIVER, Driver);
     Container.bind(
       INJECT_SRC_DIRS,
       options.srcDirs || [path.join(process.cwd(), './src')]
@@ -33,7 +57,7 @@ export async function boostrap<M>(options: BootstrapOptions<M>) {
     const foxxFactory = Container.factory(FoxxFactory);
     await foxxFactory.initRoutes();
     console.error();
-    const instance = Container.factory(options.Driver);
+    const instance = Container.factory<FoxxDriver>(Driver);
     instance.bootstrap();
     console.log('Foxx server started success!');
   } catch (err) {
