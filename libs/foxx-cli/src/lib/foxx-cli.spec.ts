fe4c62b1072a97966d5131206efa1cb2c7854ead
dyk <359743984@qq.com>
2021-08-14 15:07:27 +0800

    rm: drop lib

--- libs/foxx-cli/src/lib/foxx-cli.spec.ts
+++
@@ -1,7 +0,0 @@
-import { foxxCli } from './foxx-cli';
-
-describe('foxxCli', () => {
-  it('should work', () => {
-    expect(foxxCli()).toEqual('foxx-cli');
-  });
-});
--- libs/foxx-cli/src/lib/foxx-cli.ts
+++
@@ -1,3 +0,0 @@
-export function foxxCli(): string {
-  return 'foxx-cli';
-}
