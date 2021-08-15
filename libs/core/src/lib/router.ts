513bf9698e8c4bf6fe469f65d65f8b07bde1c21d
dyk <359743984@qq.com>
2021-08-15 11:07:56 +0800

    refactor: refactor router

--- libs/core/src/lib/router.ts
+++ libs/core/src/lib/router.ts
@@ -1,59 +1,22 @@
-import {
-  Injectable,
-  Inject,
-  TARGET_INJECTABLE,
-  Container,
-} from '@jimizai/injectable';
+import { Injectable } from '@jimizai/injectable';
 import {
   PATH_METADATA,
   METHOD_METADATA,
   PARAM_METADATA,
-  Arg,
-  RouteCollection,
 } from '@jimizai/decorators';
-import { INJECT_ROUTES } from '@jimizai/driver-types';
-import { Loader } from '@jimizai/loader';
-import { isFunction } from '@jimizai/utils';
-import { INJECT_SRC_DIRS } from '@jimizai/driver-types';
+import { CollectionFactory } from './collection';
+import { Route } from '@jimizai/driver-types';
 import * as path from 'path';
-
-export interface Route {
-  method: string;
-  url: string;
-  funcName: string;
-  identity: string;
-  args: Arg[];
-}
 
 @Injectable()
 export class Router {
-  constructor(
-    @Inject(INJECT_SRC_DIRS) private srcDirs: string | string[],
-    private loader: Loader
-  ) {}
+  constructor(private collectionFactory: CollectionFactory) {}
 
-  private async loadModules(): Promise<{ new <T>(): T }[]> {
-    const m = await this.loader.load(this.srcDirs);
-    const injectableModulesObject = m
-      .filter(
-        (module) =>
-          isFunction(module) && Reflect.getMetadata(TARGET_INJECTABLE, module)
-      )
-      .reduce(
-        (prev, target) => ({ ...prev, [target.name]: target }),
-        {} as Record<string, { new <T>(): T }>
-      );
-
-    return Object.keys(injectableModulesObject).map(
-      (key) => injectableModulesObject[key]
-    );
-  }
-
-  async initRoutes(): Promise<void> {
-    await this.loadModules();
-    const modules = RouteCollection.getModules();
-    const routes = modules
-      .map((target) => {
+  async initRoutes(): Promise<Route[]> {
+    await this.collectionFactory.initModules();
+    return this.collectionFactory
+      .getControllers()
+      .map(({ target, instance }) => {
         const prefixPath = Reflect.getMetadata(PATH_METADATA, target);
         const descriptors = Object.getOwnPropertyDescriptors(target.prototype);
         const methodKeys = Object.keys(descriptors);
@@ -77,14 +40,12 @@ export class Router {
               args: args.sort((x, y) => x.parameterIndex - y.parameterIndex),
               funcName: methodName,
               identity: target.name,
-              instance: Container.factory(target),
+              instance,
               target,
             };
           }
         }
       })
       .filter(Boolean);
-
-    Container.bind(INJECT_ROUTES, routes);
   }
 }
