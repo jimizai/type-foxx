914abb6a083085e9d82fa77dcb7a9efb066e5f96
dyk <359743984@qq.com>
2021-08-15 11:09:42 +0800

    rm: drop route collection

--- libs/decorators/src/lib/controller.ts
+++ libs/decorators/src/lib/controller.ts
@@ -2,22 +2,10 @@ import { PATH_METADATA } from './constants';
 import { normalizePath } from '@jimizai/utils';
 import { Injectable } from '@jimizai/injectable';
 
-export class RouteCollection {
-  static routeModules = [];
-
-  static setModule(m) {
-    this.routeModules.push(m);
-  }
-  static getModules() {
-    return this.routeModules;
-  }
-}
-
 export function Controller(): ClassDecorator;
 export function Controller(prefix: string): ClassDecorator;
 export function Controller(prefix = '/'): ClassDecorator {
   return (target) => {
-    RouteCollection.setModule(target);
     Injectable()(target);
     prefix = normalizePath(prefix);
     Reflect.defineMetadata(PATH_METADATA, prefix, target);
