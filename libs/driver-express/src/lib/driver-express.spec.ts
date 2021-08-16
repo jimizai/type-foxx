b5f9962a2f17186b861f6d133f793cb5a7b6afab
dyk <359743984@qq.com>
2021-08-16 21:25:05 +0800

    rm: drop driver express test

--- libs/driver-express/src/lib/driver-express.spec.ts
+++
@@ -1,7 +0,0 @@
-import { driverExpress } from './driver-express';
-
-describe('driverExpress', () => {
-  it('should work', () => {
-    expect(driverExpress()).toEqual('driver-express');
-  });
-});
