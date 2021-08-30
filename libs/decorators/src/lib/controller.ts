b1f67b3fae434bf35d93ce2f0befe90f34b28822
dyk <359743984@qq.com>
2021-08-30 22:07:09 +0800

    feat: add request scope to controller decorator

--- libs/decorators/src/lib/controller.ts
+++ libs/decorators/src/lib/controller.ts
@@ -1,12 +1,12 @@
 import { PATH_METADATA } from './constants';
 import { normalizePath } from '@jimizai/utils';
-import { Injectable } from '@jimizai/injectable';
+import { Injectable, ScopeEnum } from '@jimizai/injectable';
 
 export function Controller(): ClassDecorator;
 export function Controller(prefix: string): ClassDecorator;
 export function Controller(prefix = '/'): ClassDecorator {
   return (target) => {
-    Injectable()(target);
+    Injectable({ scope: ScopeEnum.Request })(target);
     prefix = normalizePath(prefix);
     Reflect.defineMetadata(PATH_METADATA, prefix, target);
   };
