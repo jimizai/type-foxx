7ba89106a1c99005ebd4d8247f4ff4951f97bcdf
dyk <359743984@qq.com>
2021-08-31 22:34:30 +0800

    rm: drop curd service

--- apps/blog/src/main.ts
+++ apps/blog/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from '@jimizai/core';
 import { KoaFoxxDriver } from '@jimizai/driver-koa';
 import { ORMService } from '@app/shared/orm.service';
-import { CurdService } from '@app/shared/curd';
 import '@app/exceptions';
 import '@app/modules';
 
@@ -13,7 +12,6 @@ async function main() {
   });
 
   app.get(ORMService);
-  app.get(CurdService);
   app.bootstrap();
 }
 
