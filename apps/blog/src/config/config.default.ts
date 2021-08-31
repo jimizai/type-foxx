64d14f111890ad72bf2101235b7cafa3da89e92c
dyk <359743984@qq.com>
2021-08-31 22:36:39 +0800

    chore: add orm entities

--- apps/blog/src/config/config.default.ts
+++ apps/blog/src/config/config.default.ts
@@ -1,12 +1,18 @@
+import { ConnectionOptions } from 'typeorm';
+import { BlogEntity } from '@app/modules/blog/blog.entity';
+
+const orm: ConnectionOptions = {
+  type: 'mysql',
+  host: 'test',
+  port: 3306,
+  username: 'root',
+  password: '123456',
+  database: 'blog',
+  logging: true,
+  entities: [BlogEntity],
+};
+
 export default {
   appName: 'blog',
-  orm: {
-    type: 'mysql',
-    host: 'test',
-    port: 3306,
-    username: 'root',
-    password: '123456',
-    database: 'blog',
-    logging: true,
-  },
+  orm,
 };
