53b36543b42e48557e819b98514f2499e5de986a
dyk <359743984@qq.com>
2021-09-01 23:03:30 +0800

    feat: add jwt service

--- libs/jwt/src/lib/jwt.spec.ts
+++
@@ -1,7 +0,0 @@
-import { jwt } from './jwt';
-
-describe('jwt', () => {
-  it('should work', () => {
-    expect(jwt()).toEqual('jwt');
-  });
-});
--- libs/jwt/src/lib/jwt.ts
+++ libs/jwt/src/lib/jwt.ts
@@ -1,3 +1,100 @@
-export function jwt(): string {
-  return 'jwt';
+import * as jsonwebtoken from 'jsonwebtoken';
+import { ConfigService } from '@jimizai/config';
+import { Inject, Injectable } from '@jimizai/injectable';
+
+export interface JwtConfigs {
+  jwt: JwtOpitons;
+}
+
+export interface JwtOpitons {
+  sign?: any;
+  verify?: any;
+  secret: string;
+  extras?: {
+    ttl: number; // hour
+  };
+}
+
+@Injectable()
+export class JWTService<T extends JwtConfigs> {
+  @Inject()
+  private configService: ConfigService<T>;
+
+  public get jwtConfigs() {
+    return this.configService.get('jwt');
+  }
+
+  public get secret() {
+    return this.jwtConfigs.secret;
+  }
+
+  get ttl() {
+    return (this.jwtConfigs.extras?.ttl || 7 * 24) * 60 * 60;
+  }
+
+  private jwtSign(
+    payload: any,
+    secret: string,
+    options?: any,
+    callback?: () => any
+  ): string | void {
+    if (typeof secret !== 'string') {
+      callback = options;
+      options = secret || {};
+      secret = this.secret;
+    } else if (typeof options === 'function') {
+      callback = options;
+      options = {};
+    }
+
+    return jsonwebtoken.sign(
+      payload,
+      secret,
+      Object.assign({}, this.jwtConfigs.sign || {}, options),
+      callback
+    );
+  }
+
+  private jwtVerify(
+    token: any,
+    secret: string,
+    options?: any,
+    callback?: () => any
+  ): string | void {
+    if (typeof secret !== 'string') {
+      callback = options;
+      options = secret || {};
+      secret = this.jwtConfigs.secret;
+    } else if (typeof options === 'function') {
+      callback = options;
+      options = {};
+    }
+
+    return jsonwebtoken.verify(
+      token,
+      secret,
+      Object.assign({}, this.jwtConfigs.verify || {}, options),
+      callback
+    );
+  }
+
+  async verify(token: string) {
+    const result = await this.jwtVerify(token, this.secret, {
+      clockTolerance: 30, // 检查 nbf (token 最早可用时间) 和 exp (token 过期时间) 声明时容忍的秒数，以处理不同服务器之间的小时钟差异
+    });
+
+    return result;
+  }
+
+  async create(data: any): Promise<string> {
+    return this.jwtSign({ customClaims: data }, this.secret, {
+      expiresIn: this.ttl, // token 过期时间, 单位: 小时
+    }) as string;
+  }
+
+  async getCustomClaims(token: string) {
+    const jwtData: any = await this.verify(token);
+
+    return jwtData.customClaims;
+  }
 }
