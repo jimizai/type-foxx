d9dd19205bb5531c7aee4cb7c184c5fbcfd0ae9e
dyk <359743984@qq.com>
2021-09-02 22:21:44 +0800

    feat: add guards decorator

--- libs/decorators/src/lib/guard.ts
+++ libs/decorators/src/lib/guard.ts
@@ -1,11 +1,12 @@
 import { GUARDS_METADATA, DEFAULT_GUARDS } from './constants';
+type Type<T> = { new (...args: any[]): T };
 
 export interface CanActivate {
   canActivate(context: any): boolean | Promise<boolean>;
   canActivate(req: any, res: any): boolean | Promise<boolean>;
 }
 
-export function UseGuards<T extends CanActivate>(
+export function UseGuards<T extends Type<CanActivate>>(
   ...args: T[]
 ): ClassDecorator & MethodDecorator {
   return (target, key?: string) => {
@@ -17,5 +18,16 @@ export function UseGuards<T extends CanActivate>(
       key = DEFAULT_GUARDS;
     }
     guardsObject[key] = [...new Set([...(guardsObject[key] || []), ...args])];
+    Reflect.defineMetadata(GUARDS_METADATA, guardsObject, target);
   };
 }
+
+export const getGuards = (target, key: string): Type<CanActivate>[] => {
+  const guardsObject = Reflect.getMetadata(GUARDS_METADATA, target) || {};
+  return [
+    ...new Set([
+      ...(guardsObject[key] || []),
+      ...(guardsObject[DEFAULT_GUARDS] || []),
+    ]),
+  ];
+};
