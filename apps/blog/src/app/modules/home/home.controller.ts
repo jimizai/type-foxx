befbeb78aa1ef1fc50cd7cf65adead0be5d7ba1c
dyk <359743984@qq.com>
2021-09-02 22:23:34 +0800

    chore: test useGuards

--- apps/blog/src/app/modules/home/home.controller.ts
+++ apps/blog/src/app/modules/home/home.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@jimizai/decorators';
+import { Controller, Get, UseGuards } from '@jimizai/decorators';
 import { ConfigService } from '@jimizai/config';
 import { BaseController } from '@jimizai/curd';
 import { Inject } from '@jimizai/injectable';
+import { AuthGuard } from '@app/guards/auth.guard';
 import { FoxxConfig } from '@/config';
 
 @Controller()
@@ -10,7 +11,13 @@ export class HomeController extends BaseController {
   private configService: ConfigService<FoxxConfig>;
 
   @Get()
+  @UseGuards(AuthGuard)
   home() {
+    this.setData(this.configService.get('appName')).succeed();
+  }
+
+  @Get('/test')
+  test() {
     this.setData(this.configService.get('appName')).succeed();
   }
 }
