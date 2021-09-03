eb7812c36f7bd910f0098b11f47572a6fb126ed9
dyk <359743984@qq.com>
2021-09-03 22:31:55 +0800

    feat: add auth guard

--- apps/blog/src/app/guards/auth.guard.ts
+++ apps/blog/src/app/guards/auth.guard.ts
@@ -1,11 +1,21 @@
 import { CanActivate } from '@jimizai/decorators';
-import { Injectable } from '@jimizai/injectable';
+import { Inject, Injectable } from '@jimizai/injectable';
+import { BaseExceptions } from '@jimizai/common';
+import { JWTService } from '@jimizai/jwt';
 import { Context } from 'koa';
+import { FoxxConfig } from '@/config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(ctx: Context): boolean {
-    console.log(ctx);
-    return false;
+  @Inject()
+  jWTService: JWTService<FoxxConfig>;
+
+  async canActivate(ctx: Context): Promise<boolean> {
+    try {
+      await this.jWTService.verify(ctx.headers.authorization);
+    } catch {
+      throw new BaseExceptions('Forbidden', 403, 'Permission denied');
+    }
+    return true;
   }
 }
