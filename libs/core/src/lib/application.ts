a3f91ee0bc82e241b37f4da53d0ae6c1e9091142
dyk <359743984@qq.com>
2021-09-04 13:13:05 +0800

    feat: add global guards and pipes

--- libs/core/src/lib/application.ts
+++ libs/core/src/lib/application.ts
@@ -5,13 +5,32 @@ import {
   FoxxDriver,
 } from '@jimizai/driver-types';
 import { FactoryContainer, Inject } from '@jimizai/injectable';
+import { PipeTransform, CanActivate } from '@jimizai/decorators';
 
 @Driver()
 export class FoxxApplicationImpl implements FoxxApplication {
   constructor(@Inject(INJECT_FOXX_DRIVER) private driver: FoxxDriver) {}
+  private pipes: PipeTransform[];
+  private guards: CanActivate[] = [];
 
   get<T>(target: { new (...args: any[]): T }): T {
     return FactoryContainer.factory<T>(target);
+  }
+
+  useGlobalPipes(...pipes: PipeTransform[]) {
+    this.pipes = pipes;
+  }
+
+  getGlobalPipes() {
+    return this.pipes;
+  }
+
+  useGlobalGuards(...guards: CanActivate[]) {
+    this.guards = guards;
+  }
+
+  getGlobalGuards() {
+    return this.guards;
   }
 
   bootstrap(): void {
