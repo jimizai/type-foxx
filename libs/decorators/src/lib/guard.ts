04fcd7890cce69f99aec219b05e67ec262d2d746
dyk <359743984@qq.com>
2021-09-04 13:14:17 +0800

    refactor: rewrite use guards

--- libs/decorators/src/lib/guard.ts
+++ libs/decorators/src/lib/guard.ts
@@ -1,33 +1,19 @@
-import { GUARDS_METADATA, DEFAULT_GUARDS } from './constants';
-type Type<T> = { new (...args: any[]): T };
+import { CanActivate } from './interface';
+import { setMethodMetadata } from './utils';
+import { FactoryContainer } from '@jimizai/injectable';
 
-export interface CanActivate {
-  canActivate(context: any): boolean | Promise<boolean>;
-  canActivate(req: any, res: any): boolean | Promise<boolean>;
-}
+type Type<T> = { new (...args: any[]): T };
 
 export function UseGuards<T extends Type<CanActivate>>(
   ...args: T[]
 ): ClassDecorator & MethodDecorator {
   return (target, key?: string) => {
     if (key) {
-      target = target.constructor;
+      setMethodMetadata(target.constructor, key, {
+        guards: args.map((arg) => FactoryContainer.factory(arg)),
+      });
+    } else {
+      //
     }
-    const guardsObject = Reflect.getMetadata(GUARDS_METADATA, target) || {};
-    if (!key) {
-      key = DEFAULT_GUARDS;
-    }
-    guardsObject[key] = [...new Set([...(guardsObject[key] || []), ...args])];
-    Reflect.defineMetadata(GUARDS_METADATA, guardsObject, target);
   };
 }
-
-export const getGuards = (target, key: string): Type<CanActivate>[] => {
-  const guardsObject = Reflect.getMetadata(GUARDS_METADATA, target) || {};
-  return [
-    ...new Set([
-      ...(guardsObject[key] || []),
-      ...(guardsObject[DEFAULT_GUARDS] || []),
-    ]),
-  ];
-};
