ccf3a986bf6f6d9419d8bba09e0b0b735a7774c7
jimizai <359743984@qq.com>
2021-10-31 15:45:47 +0800

    feat: support lazy comsumer

--- libs/async-pool/src/lib/async-pool.spec.ts
+++ libs/async-pool/src/lib/async-pool.spec.ts
@@ -1,15 +1,20 @@
 import { AsyncPool } from './async-pool';
 import { FactoryContainer } from '@jimizai/injectable';
+import { sleep } from '@jimizai/utils';
 
 describe('asyncPool', () => {
   it('should work', async () => {
     const asyncPool =
-      FactoryContainer.factory<AsyncPool<string, void>>(AsyncPool);
+      FactoryContainer.factory<AsyncPool<string, string>>(AsyncPool);
     const list = ['test', 'test2', 'test3', 'test4'];
     asyncPool.merge(list);
     asyncPool.execute((str: string) => {
       expect(list.includes(str)).toBe(true);
+      return str;
     });
     await asyncPool.run();
+    asyncPool.merge(list);
+    await sleep(100);
+    expect(asyncPool.getResults().length).toBe(8);
   });
 });
--- libs/async-pool/src/lib/async-pool.ts
+++ libs/async-pool/src/lib/async-pool.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@jimizai/injectable';
+import { Init } from '@jimizai/decorators';
 import asyncPool from 'tiny-async-pool';
+
+const WAKEUP_METHODS = ['push'];
 
 @Injectable()
 export class AsyncPool<T, K> {
   private list: T[] = [];
   private fn: (data: T) => Promise<K> | K;
+  private running = false;
+  private results: K[] = [];
+  private jobs = 2;
+
+  @Init()
+  protected AsyncPool() {
+    const arrExtend = Object.create(Array.prototype);
+    // eslint-disable-next-line
+    const self = this;
+    WAKEUP_METHODS.forEach((method) => {
+      const oldMethod = Array.prototype[method];
+      arrExtend[method] = function (...args) {
+        oldMethod.apply(this, args);
+        self.wake();
+      };
+    });
+
+    (this.list as any).__proto__ = arrExtend;
+  }
+
+  getResults() {
+    return this.results;
+  }
 
   push(data: T) {
     this.list.push(data);
@@ -14,11 +40,26 @@ export class AsyncPool<T, K> {
     this.list.push(...arr);
   }
 
-  execute(fn: (data: T) => Promise<K> | K) {
-    this.fn = fn;
+  private wake() {
+    if (this.running) {
+      return;
+    }
+    this.run();
   }
 
-  async run(jobs = 2) {
-    return asyncPool(jobs, this.list, this.fn);
+  execute(fn: (data: T) => Promise<K> | K) {
+    this.fn = fn;
+    this.wake();
+  }
+
+  async run(jobs?: number) {
+    if (!this.fn || this.running) return;
+    if (jobs) {
+      this.jobs = jobs;
+    }
+    this.running = true;
+    const res = await asyncPool(this.jobs, this.list, this.fn);
+    this.results.push(...res);
+    this.running = false;
   }
 }
