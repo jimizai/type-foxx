abc3f1f57fb50a538195876e9540a4132aafc265
jimizai <359743984@qq.com>
2021-12-21 22:30:21 +0800

    refactor: refactor driver koa middleware

--- libs/driver-koa/src/lib/driver-koa.ts
+++ libs/driver-koa/src/lib/driver-koa.ts
@@ -1,6 +1,11 @@
 import { NotFoundException, BaseExceptions } from '@jimizai/common';
 import { ArgType, PARAM_ALL, Driver } from '@jimizai/decorators';
-import { Inject } from '@jimizai/injectable';
+import {
+  Inject,
+  Injectable,
+  Typed,
+  FactoryContainer,
+} from '@jimizai/injectable';
 import { isUndefined } from '@jimizai/utils';
 import {
   FoxxDriver,
@@ -25,7 +30,70 @@ type ExtendInterface<T> = {
 export interface FoxxContext
   extends Context,
     ExtendInterface<typeof ExtendContext> {
+  //eslint-disable-next-line
   requestContext: { get<T>(target: { new (...args: any[]): T }): T };
+}
+
+export interface FoxxMiddleware {
+  call: Middleware;
+}
+
+@Injectable()
+class ExtendContextMiddleware implements FoxxMiddleware {
+  constructor(@Inject(INJECT_OPEN_API) private openApi: OpenApi) {
+    //
+  }
+
+  async call(ctx, next) {
+    Object.assign(ctx, {
+      requestContext: this.openApi,
+    });
+    Object.assign(ctx, ExtendContext) as FoxxContext;
+    await next();
+  }
+}
+
+@Injectable()
+class ExtendRouterArgsMiddleware implements FoxxMiddleware {
+  constructor(@Inject(INJECT_OPEN_API) private openApi: OpenApi) {
+    //
+  }
+  async call(ctx, next) {
+    this.openApi.setRouterArgs({ ctx, req: ctx.request, res: ctx.response });
+    await next();
+  }
+}
+
+@Injectable()
+class ErrorHandlerMiddleware implements FoxxMiddleware {
+  constructor(
+    @Inject(INJECT_CATCHERS)
+    private catchers: Catchers<
+      <T extends BaseExceptions>(error: T, ctx: FoxxContext) => void
+    >
+  ) {
+    //
+  }
+  async call(ctx, next) {
+    try {
+      await next();
+      if (isUndefined(ctx.body) && ctx.status === 404) {
+        throw new NotFoundException();
+      }
+    } catch (error) {
+      if (!Object.keys(this.catchers).length) {
+        console.error(error);
+        ctx.body = {
+          code: error.getStatus?.() || 500,
+          message: error.getMessage?.() || 'Internal server error',
+        };
+      } else {
+        const catcherName = error.name || 'default';
+        const callback = this.catchers[catcherName] || this.catchers['default'];
+        callback?.(error, ctx);
+      }
+    }
+  }
 }
 
 @Driver()
@@ -33,71 +101,20 @@ export class KoaFoxxDriver implements FoxxDriver {
   public instance: Koa;
   public routerInstance: Router;
 
-  private globalMiddlewares: Middleware[] = [
-    this.extendContext(),
-    this.extendRouterArgs(),
-    this.errorHandler(),
+  private globalMiddlewares: Typed<FoxxMiddleware>[] = [
+    ExtendContextMiddleware,
+    ExtendRouterArgsMiddleware,
+    ErrorHandlerMiddleware,
   ];
 
   constructor(
     @Inject(INJECT_SERVER_PORT) private port: number = 7001,
-    @Inject(INJECT_FOXX_MIDDLEWARES) middlewares: Middleware[] = [],
-    @Inject(INJECT_ROUTES) private routes: Route[] = [],
-    @Inject(INJECT_OPEN_API) private openApi: OpenApi,
-    @Inject(INJECT_CATCHERS)
-    private catchers: Catchers<
-      <T extends BaseExceptions>(error: T, ctx: FoxxContext) => void
-    >
+    @Inject(INJECT_FOXX_MIDDLEWARES) middlewares: Typed<FoxxMiddleware>[] = [],
+    @Inject(INJECT_ROUTES) private routes: Route[] = []
   ) {
     this.instance = new Koa();
     this.routerInstance = new Router();
     this.globalMiddlewares = [...this.globalMiddlewares, ...middlewares];
-  }
-
-  private use(middleware: Middleware) {
-    this.globalMiddlewares.push(middleware);
-    return this;
-  }
-
-  private extendRouterArgs() {
-    return async (ctx, next) => {
-      this.openApi.setRouterArgs({ ctx, req: ctx.request, res: ctx.response });
-      await next();
-    };
-  }
-
-  private extendContext() {
-    return async (ctx, next) => {
-      Object.assign(ctx, {
-        requestContext: this.openApi,
-      });
-      Object.assign(ctx, ExtendContext) as FoxxContext;
-      await next();
-    };
-  }
-
-  private errorHandler() {
-    return async (ctx: FoxxContext, next) => {
-      try {
-        await next();
-        if (isUndefined(ctx.body) && ctx.status === 404) {
-          throw new NotFoundException();
-        }
-      } catch (error) {
-        if (!Object.keys(this.catchers).length) {
-          console.error(error);
-          ctx.body = {
-            code: error.getStatus?.() || 500,
-            message: error.getMessage?.() || 'Internal server error',
-          };
-        } else {
-          const catcherName = error.name || 'default';
-          const callback =
-            this.catchers[catcherName] || this.catchers['default'];
-          callback?.(error, ctx);
-        }
-      }
-    };
   }
 
   private useRoutes() {
@@ -122,7 +139,8 @@ export class KoaFoxxDriver implements FoxxDriver {
               arg.argType === ArgType.Query
                 ? ctx.query
                 : arg.argType === ArgType.Body
-                ? (ctx.request as any).body
+                ? // eslint-disable-next-line
+                  (ctx.request as any).body
                 : ctx.params;
             if (!arg.name) {
               return;
@@ -143,17 +161,18 @@ export class KoaFoxxDriver implements FoxxDriver {
         }
       );
     });
-    this.use(this.routerInstance.routes()).use(
-      this.routerInstance.allowedMethods()
-    );
     return this;
   }
 
   public bootstrap() {
     this.useRoutes();
-    this.globalMiddlewares.forEach((middleware) => {
-      this.instance.use(middleware);
+    this.globalMiddlewares.forEach((foxxMiddleware) => {
+      const middleware = FactoryContainer.factory(foxxMiddleware);
+      this.instance.use(middleware.call.bind(middleware));
     });
+    this.instance
+      .use(this.routerInstance.routes())
+      .use(this.routerInstance.allowedMethods());
     this.instance.listen(this.port, () => {
       console.log('server started on http://localhost:' + this.port);
     });
